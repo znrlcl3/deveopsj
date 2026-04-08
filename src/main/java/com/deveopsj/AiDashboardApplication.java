05b2ead6443a8014b6f29bb17f9c12ecb49ecdfd
이성진 <znrlcl3@naver.com>
2026-04-08 22:51:22 +0900

    chore: Spring Boot 초기 세팅 및 DB 설계 반영

---
+++ src/main/java/com/deveopsj/AiDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.deveopsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDashboardApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/deveopsj/repository/AssetPlanRepository.java
@@ -0,0 +1,7 @@
+package com.deveopsj.repository;
+
+import com.deveopsj.entity.AssetPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssetPlanRepository extends JpaRepository<AssetPlan, Integer> {
+}
